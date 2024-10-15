ed33bc48e5e1bc870b06d3fcd9b8accbc9dc74b4
Joshua Flowers <flowers.joshuac@gmail.com>
2024-10-15 13:34:57 -0500

    Added LSP keymaps

--- lua/config/autocommands.lua
+++ lua/config/autocommands.lua
@@ -1,6 +1,6 @@
-local file_au_group = vim.api.nvim_create_augroup("init.lua", { clear = true })
+local file_au_group = vim.api.nvim_create_augroup("autocommands.lua", { clear = true })
 
-function remove_eob_fills(args)
+local function remove_eob_fills()
 	vim.cmd("setlocal fillchars=eob:\\ ")
 end
 
@@ -13,7 +13,21 @@ vim.api.nvim_create_autocmd("BufWinEnter", {
 	group = file_au_group,
 	callback = function(args)
 		if vim.bo[args.buf].filetype == "neo-tree" then
-			remove_eob_fills(args)
+			remove_eob_fills()
+		end
+	end,
+})
+
+vim.api.nvim_create_autocmd("LspAttach", {
+	group = file_au_group,
+	callback = function(args)
+		local bufnr = args.buf
+		local client = vim.lsp.get_client_by_id(args.data.client_id)
+		if client.server_capabilities.definitionProvider then
+			vim.keymap.set("n", "<Leader>ld", vim.lsp.buf.definition, { buffer = bufnr, desc = "Goto definition" })
+		end
+		if client.server_capabilities.hoverProvider then
+			vim.keymap.set("n", "<Leader>lh", vim.lsp.buf.hover, { buffer = bufnr, desc = "Hover popup" })
 		end
 	end,
 })
