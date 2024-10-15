22588d6cb327ffd678fc79e0bb6115940a89ca1e
Joshua Flowers <flowers.joshuac@gmail.com>
2024-10-15 00:09:51 -0500

    Made neo-tree no longer lazy
    This makes it be the default view when I open a directory with neovim

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -7,6 +7,7 @@ return {
 		  "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
 		  "MunifTanjim/nui.nvim",
 		},
+		lazy = false,
 		keys = {
 			{ "<Leader>e", ":Neotree<CR>", desc = "Open Neotree" },
 		},
