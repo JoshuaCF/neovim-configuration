93fe5fbae03631c5b5bf616693a45127e961ef5f
Joshua Flowers <flowers.joshuac@gmail.com>
2024-10-14 23:37:39 -0500

    Fleshed out treesitter configurations

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,5 +1,10 @@
 return {
 	{
-		"nvim-treesitter/nvim-treesitter", opts = {}
+		"nvim-treesitter/nvim-treesitter", opts = {
+			ensure_installed = { "rust", "lua", "c", "cpp" },
+			highlight = { enable = true },
+			incremental_selection = { enable = true },
+		},
+		main = "nvim-treesitter.configs",
 	}
 }
