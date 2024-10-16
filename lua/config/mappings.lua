b42781402d54167e40f96f9e7734bddf9612b020
Joshua Flowers <flowers.joshuac@gmail.com>
2024-10-15 19:59:06 -0500

    Added mappings involving paired characters like quotes and brackets

--- lua/config/mappings.lua
+++ lua/config/mappings.lua
@@ -1,14 +1,49 @@
+-- LSP Bindings are in autocommands.lua
+
+-- Misc mappings
 vim.keymap.set("n", "<Leader>tl", function()
 	vim.opt.relativenumber = not vim.opt.relativenumber:get()
 end, { desc = "Toggle relative line numbers" })
+vim.keymap.set("n", "<Esc>", ":noh<CR>", { desc = "Remove search highlights" })
 
+-- Buffer navigation mappings
 vim.keymap.set("n", "<Leader>x", ":bp|bd#<CR>",
-	{ desc = "Close the current buffer and move previous buffer into window" }
+	{ desc = "Close buffer" }
 )
 vim.keymap.set("n", "<Leader>X", ":bp|bd!#<CR>",
-	{ desc = "Force close the current buffer and move previous buffer into window" }
+	{ desc = "Force close buffer" }
 )
 
-vim.keymap.set("n", "<ESC>", ":noh<CR>", { desc = "Remove search highlights" })
+-- Surround mappings
+vim.keymap.set("v", "<Leader>(", "<Esc>`>a)<Esc>`<lt>i(<Esc>",
+	{ desc = "Surround selection with parentheses" }
+)
+vim.keymap.set("v", "<Leader>[", "<Esc>`>a]<Esc>`<lt>i[<Esc>",
+	{ desc = "Surround selection with square brackets" }
+)
+vim.keymap.set("v", "<Leader>{", "<Esc>`>a}<Esc>`<lt>i{<Esc>",
+	{ desc = "Surround selection with curly brackets" }
+)
+vim.keymap.set("v", "<Leader>'", "<Esc>`>a'<Esc>`<lt>i'<Esc>",
+	{ desc = "Surround selection with single quotes" }
+)
+vim.keymap.set("v", "<Leader>\"", "<Esc>`>a\"<Esc>`<lt>i\"<Esc>",
+	{ desc = "Surround selection with double quotes" }
+)
 
--- LSP Bindings are in autocommands.lua
+-- Paired insert mappings
+vim.keymap.set("n", "<Leader>(", "i()<Esc>i",
+	{ desc = "Insert paired parentheses" }
+)
+vim.keymap.set("n", "<Leader>[", "i[]<Esc>i",
+	{ desc = "Insert paired square brackets" }
+)
+vim.keymap.set("n", "<Leader>{", "i{}<Esc>i",
+	{ desc = "Insert paired curly brackets" }
+)
+vim.keymap.set("n", "<Leader>'", "i''<Esc>i",
+	{ desc = "Insert paired single quotes" }
+)
+vim.keymap.set("n", "<Leader>\"", "i\"\"<Esc>i",
+	{ desc = "Insert paired double quotes" }
+)
