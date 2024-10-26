5790fd0a7f9a28e2bc91e7a67b86835ac8296086
Joshua Flowers <flowers.joshuac@gmail.com>
2024-10-25 20:07:04 -0500

    Normalized string quote style

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -3,7 +3,7 @@ return {
 	"neovim/nvim-lspconfig",
 	branch = "master",
 	config = function()
-		local capabilities = require('cmp_nvim_lsp').default_capabilities()
+		local capabilities = require("cmp_nvim_lsp").default_capabilities()
 		for _,v in pairs(servers) do
 			require("lspconfig")[v].setup({ capabilities = capabilities })
 		end
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,10 +1,10 @@
 return {
     {
-		'nvim-telescope/telescope.nvim', tag = '0.1.8',
+		"nvim-telescope/telescope.nvim", tag = "0.1.8",
 		dependencies = {
-			'nvim-lua/plenary.nvim',
-			'nvim-treesitter/nvim-treesitter',
-			'nvim-tree/nvim-web-devicons'
+			"nvim-lua/plenary.nvim",
+			"nvim-treesitter/nvim-treesitter",
+			"nvim-tree/nvim-web-devicons"
 		}
     }
 }
