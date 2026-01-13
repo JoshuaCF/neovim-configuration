1bf670ecfabefa73e266456d334d3439f72351b2
Joshua Flowers <flowers.joshuac@gmail.com>
2026-01-12 19:52:42 -0600

    Added separate binds for toggling outline view and toggling outline focus

--- lua/plugins/outline.lua
+++ lua/plugins/outline.lua
@@ -3,7 +3,18 @@ return {
 	lazy = true,
 	cmd = { "Outline", "OutlineOpen" },
 	keys = {
-		{ "<leader>o", "<cmd>Outline<CR>", desc = "Toggle outline" },
+		{ "<leader>O", "<cmd>Outline!<CR>", desc = "Toggle outline window" },
+		{ "<leader>o",
+			function()
+				local outline = require("outline")
+				if outline.is_open() then
+					outline.focus_toggle()
+				else
+					outline.open_outline({ focus_outline = true })
+				end
+			end,
+			desc = "Toggle outline focus",
+		},
 	},
 	opts = {},
 }
