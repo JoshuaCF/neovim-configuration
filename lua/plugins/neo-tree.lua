23c10f1c9b0a215187c1723df2dc848188552d0e
Joshua Flowers <flowers.joshuac@gmail.com>
2026-01-15 15:22:25 -0600

    Replaced neo-tree's fuzzy searcher with telescope fd Telescope is faster and in my opinion better for finding files Getting this to work neatly required a lot more work than expected, and using a number of (as far as I could tell) undocumented features from neo-tree. Hopefully they don't break how I did this

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,3 +1,49 @@
+local fuzzy_file_search_substitute = function()
+	-- Open telescope prompt for files and modify binds as follows:
+	-- <CR> on a file should reveal it in Neotree before opening it
+	-- <S-CR> should only reveal it in Neotree
+	local reveal_file = function(path)
+		require("neo-tree.command").execute({
+			action = "focus",
+			source = "filesystem",
+			position = "left",
+			reveal_file = path,
+		})
+	end
+
+	require("telescope.builtin").find_files({
+		attach_mappings = function(_, map)
+			map({"i", "n"}, "<CR>", function(prompt_bufnr)
+				local telescope_state = require("telescope.actions.state")
+				local actions = require("telescope.actions")
+
+				local entry = telescope_state.get_selected_entry()
+				actions.close(prompt_bufnr)
+				reveal_file(entry.cwd .. "/" .. entry[1])
+				-- Jank, but seemingly necessary. After revealing the file, the Neotree state doesn't immediately
+				-- update, and so even though the new state is fetched here, it won't work. Perhaps due to
+				-- needing to load the filesystem or something like that?
+				-- 50 milliseconds seemed to be a robust enough delay and is hardly noticeable
+				vim.defer_fn(function()
+					local nt_state = require("neo-tree.sources.manager").get_state("filesystem", nil, nil)
+					nt_state.commands.open(nt_state)
+				end, 50)
+			end, { desc = "Edit and reveal in Neotree" })
+
+			map({"i", "n"}, "<S-CR>", function(prompt_bufnr)
+				local telescope_state = require("telescope.actions.state")
+				local actions = require("telescope.actions")
+
+				local entry = telescope_state.get_selected_entry()
+				actions.close(prompt_bufnr)
+				reveal_file(entry.cwd .. "/" .. entry[1])
+			end, { desc = "Reveal in Neotree" })
+
+			return true
+		end,
+	})
+end
+
 return {
  	{
  		"nvim-neo-tree/neo-tree.nvim",
@@ -16,7 +62,13 @@ return {
  			filesystem = {
  				filtered_items = {
  					hide_dotfiles = false
- 				}
+ 				},
+				window = {
+					mappings = {
+						-- Replaces default fuzzy file search because it's slow
+						["/"] = { fuzzy_file_search_substitute, desc = "Telescope find files" },
+					},
+				},
  			}
  		},
  	}
